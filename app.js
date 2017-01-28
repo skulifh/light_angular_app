d97682f686abffd7c5480a108d90b1ae547a2b2b
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-01-28 20:04:32 +0100

    Implemented the search feature for movies. Removed the class for moviescontroller

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-class MoviesController {
-    constructor ($http, $scope) {
-        'ngInject';
-        this.$http = $http;
-        this.$scope = $scope;
-        this.getMovies();
-        //this.getMessages();
-    }
-
-    getMovies() {
-        var vm = this;
-        vm.$http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=97c9873a035726c716e8254e0a0e8ed1&language=en-US&page=1")
-	    .then(function(response) {
-	        vm.$scope.topMovies = response.data;
-	    });
-    }
-}
-
 var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
@@ -26,7 +8,8 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         .state('movies', {
             url: '/movies',
             templateUrl: 'partial-home.html',
-            controller: 'moviesController'
+            controller: 'moviesController',
+            controllerAs: 'movies'
         })
         .state('movie', {
             url: '/movies/{movieid}',
@@ -36,7 +19,20 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 });
 
 routerApp
-.controller('moviesController', MoviesController)
+.controller('moviesController', function($scope, $http) {
+	$http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=97c9873a035726c716e8254e0a0e8ed1&language=en-US&page=1")
+	.then(function(response) {
+	 	$scope.topMovies = response.data;
+	});
+
+	$scope.searchMovies = function() {
+    	$http.get("https://api.themoviedb.org/3/search/movie?api_key=97c9873a035726c716e8254e0a0e8ed1&language=en-US&query=" + $scope.query + "&page=1&include_adult=false")
+		.then(function(response) {
+		 	$scope.topMovies = response.data;
+		});
+  	}
+})
+
 .controller('movieController', function($scope, $stateParams, $http) {
     $http.get("https://api.themoviedb.org/3/movie/" + $stateParams.movieid + "?api_key=97c9873a035726c716e8254e0a0e8ed1&language=en-US")
     .then(function(response) {
