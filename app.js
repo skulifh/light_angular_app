baef5615c0dc9d98f8f29390b622723baf36b819
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-01-28 16:37:13 +0100

    now possible to click on individual movie in the top movies list which directs you to another view for that movie

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/movies',
             templateUrl: 'partial-home.html',
             controller: 'moviesController'
+        })
+        .state('movie', {
+            url: '/movies/{movieid}',
+            templateUrl: 'partial-movie.html',
+            controller: 'movieController'
         });
 });
 
-routerApp.controller('moviesController', function($scope, $http) {
+routerApp
+.controller('moviesController', function($scope, $http) {
     $http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=97c9873a035726c716e8254e0a0e8ed1&language=en-US&page=1")
     .then(function(response) {
         $scope.topMovies = response.data;
+    });
+})
+.controller('movieController', function($scope, $stateParams, $http) {
+    $http.get("https://api.themoviedb.org/3/movie/" + $stateParams.movieid + "?api_key=97c9873a035726c716e8254e0a0e8ed1&language=en-US")
+    .then(function(response) {
+        $scope.movie = response.data;
     });
 });
